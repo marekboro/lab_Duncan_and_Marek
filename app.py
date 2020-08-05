d0761f69871aeaa70523173056ff62fb0c2e845f
marekboro <marek.borowicz83@gmail.com>
2020-08-05 16:37:14 +0100

    Adding works

--- app.py
+++ app.py
@@ -8,12 +8,35 @@ from models.author import Author
 
 app = Flask(__name__)
 
+books_blueprint = Blueprint("books", __name__)
+
+@books_blueprint.route("/books")
+def books():
+    books = book_repository.select_all()
+    return render_template('books/index.html', books=books)
 
 
+@books_blueprint.route("/books/<id>/delete", methods = ['POST'])
+def delete_book(id):
+    book_repository.delete(id)
+    return redirect("/books")
 
+@books_blueprint.route("/books/new")
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", authors=authors)
 
+@books_blueprint.route("/books", methods=['POST'])
+def create_book():
+    title = request.form['title']
+    genre = request.form['genre']
+    author_id =request.form['author_id']
+    author = author_repository.select(author_id)
+    new_book = Book(title, author, genre)
+    book_repository.save(new_book)
+    return redirect("/books")
 
-
+app.register_blueprint(books_blueprint)
 
 
 @app.route('/')
--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -19,7 +19,7 @@ def select_all():
     results = run_sql(sql)
 
     for row in results:
-        author = author_repository.select(row['auhtor_id'])
+        author = author_repository.select(row['author_id'])
         book = Book(row['title'], author, row['genre'], row['id'])
         books.append(book)
     return books
